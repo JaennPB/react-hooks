a124bb4417f19a9aa441f1c38981fbe881c139cc
jaennpb <jaenn.bucio@gmail.com>
2021-04-06 13:40:24 -0500

    added useEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,15 +8,56 @@ import Search from './Search';
 const Ingredients = (props) => {
   const [currIngs, setIngs] = useState([]);
 
-  const addIngredientHandler = (ingredients) => {
-    setIngs((prevState) => [
-      ...prevState,
-      { id: Math.random().toString(), ...ingredients },
-    ]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          'https://react-hooks-5d833-default-rtdb.firebaseio.com/ingredients.json'
+        );
+        const fetchedIngData = [];
+        for (let key in res.data) {
+          fetchedIngData.push({
+            id: key,
+            amount: res.data[key].amount,
+            title: res.data[key].title,
+          });
+        }
+        setIngs(fetchedIngData);
+        console.log('Fetching ings...');
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+  // useEffect with [] as a second argument useEffect(() => {}, []) is equal to componentDidMount() and runs once after first mounting
+
+  useEffect(() => {
+    console.log('Rendering ingredients...');
+  });
+  // useEffect with no second argument useEffect(() => {}) is equal to componentDidUpdate() and runs for every render and re-render cycle
+
+  const addIngredientHandler = async (ingredients) => {
+    try {
+      const res = await axios.post(
+        'https://react-hooks-5d833-default-rtdb.firebaseio.com/ingredients.json',
+        ingredients
+      );
+      setIngs((prevState) => [
+        ...prevState,
+        { id: res.data.name, ...ingredients },
+      ]);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const deleteHandler = (id) => {
     setIngs((prevState) => prevState.filter((ing) => ing.id !== id));
+  };
+
+  const filterIngsHandler = (ingredients) => {
+    setIngs(ingredients);
   };
 
   return (
@@ -23,7 +65,7 @@ const Ingredients = (props) => {
       <IngredientForm addIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search filterIngs={filterIngsHandler} />
         <IngredientList ingredients={currIngs} onRemoveItem={deleteHandler} />
       </section>
     </div>
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import Card from '../UI/Card';
 import './Search.css';
 
-const Search = React.memo(props => {
+const Search = React.memo((props) => {
+  const { filterIngs } = props;
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const query =
+          search.length === 0 ? '' : `?orderBy="title"&equalTo="${search}"`;
+        const res = await axios.get(
+          'https://react-hooks-5d833-default-rtdb.firebaseio.com/ingredients.json' +
+            query
+        );
+        const fetchedIngData = [];
+        for (let key in res.data) {
+          fetchedIngData.push({
+            id: key,
+            amount: res.data[key].amount,
+            title: res.data[key].title,
+          });
+        }
+        // filterIngs(fetchedIngData);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [search, filterIngs]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={search}
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+          />
         </div>
       </Card>
     </section>
