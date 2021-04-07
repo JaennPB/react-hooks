f52a15f29d795a9180a7d9d1211e93c0ad3d7cf7
jaennpb <jaenn.bucio@gmail.com>
2021-04-06 19:11:14 -0500

    added more usestate funtions

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,36 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
+import ErrorModal from '../UI/ErrorModal';
 
 const Ingredients = (props) => {
-  const [currIngs, setIngs] = useState([]);
+  const [ings, setIngs] = useState([]);
+  const [loading, setloading] = useState(false);
+  const [error, setError] = useState();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(
-          'https://react-hooks-5d833-default-rtdb.firebaseio.com/ingredients.json'
-        );
-        const fetchedIngData = [];
-        for (let key in res.data) {
-          fetchedIngData.push({
-            id: key,
-            amount: res.data[key].amount,
-            title: res.data[key].title,
-          });
-        }
-        setIngs(fetchedIngData);
-        console.log('Fetching ings...');
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchData();
-  }, []);
-  // useEffect with [] as a second argument useEffect(() => {}, []) is equal to componentDidMount() and runs once after first mounting
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const res = await axios.get(
+  //         'https://react-hooks-5d833-default-rtdb.firebaseio.com/ingredients.json'
+  //       );
+  //       const fetchedIngData = [];
+  //       for (let key in res.data) {
+  //         fetchedIngData.push({
+  //           id: key,
+  //           amount: res.data[key].amount,
+  //           title: res.data[key].title,
+  //         });
+  //       }
+  //       setIngs(fetchedIngData);
+  //       console.log('Fetching ings...');
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
+  // // useEffect with [] as a second argument useEffect(() => {}, []) is equal to componentDidMount() and runs once after first mounting
 
   useEffect(() => {
     console.log('Rendering ingredients...');
@@ -39,34 +42,55 @@ const Ingredients = (props) => {
 
   const addIngredientHandler = async (ingredients) => {
     try {
+      setloading(true);
       const res = await axios.post(
         'https://react-hooks-5d833-default-rtdb.firebaseio.com/ingredients.json',
         ingredients
       );
+      setloading(false);
       setIngs((prevState) => [
         ...prevState,
         { id: res.data.name, ...ingredients },
       ]);
     } catch (err) {
-      console.log(err);
+      setError('Something went wrong!! 😯💥');
+      setloading(false);
     }
   };
 
-  const deleteHandler = (id) => {
-    setIngs((prevState) => prevState.filter((ing) => ing.id !== id));
+  const deleteHandler = async (id) => {
+    try {
+      setloading(true);
+      await axios.delete(
+        `https://react-hooks-5d833-default-rtdb.firebaseio.com/ingredients/${id}.json`
+      );
+      setloading(false);
+      setIngs((prevState) => prevState.filter((ing) => ing.id !== id));
+    } catch (err) {
+      // set state that are together in the same synchronous execution cycle (e.g. in the same function)
+      // will NOT trigger two component re-render cycles
+      setError('Something went wrong!! 😯💥');
+      setloading(false);
+      // Instead, the component will only re-render once and both state updates will be applied simultaneously.
+    }
   };
 
-  const filterIngsHandler = (ingredients) => {
+  const filterIngsHandler = useCallback((ingredients) => {
     setIngs(ingredients);
+  }, []);
+
+  const modalCloseHandler = () => {
+    setError(null);
   };
 
   return (
     <div className="App">
-      <IngredientForm addIngredient={addIngredientHandler} />
+      {error && <ErrorModal onClose={modalCloseHandler}>{error}</ErrorModal>}
+      <IngredientForm addIngredient={addIngredientHandler} loading={loading} />
 
       <section>
         <Search filterIngs={filterIngsHandler} />
-        <IngredientList ingredients={currIngs} onRemoveItem={deleteHandler} />
+        <IngredientList ingredients={ings} onRemoveItem={deleteHandler} />
       </section>
     </div>
   );
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 
 import Card from '../UI/Card';
@@ -7,31 +7,41 @@ import './Search.css';
 const Search = React.memo((props) => {
   const { filterIngs } = props;
   const [search, setSearch] = useState('');
+  const inputRef = useRef();
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const query =
-          search.length === 0 ? '' : `?orderBy="title"&equalTo="${search}"`;
-        const res = await axios.get(
-          'https://react-hooks-5d833-default-rtdb.firebaseio.com/ingredients.json' +
-            query
-        );
-        const fetchedIngData = [];
-        for (let key in res.data) {
-          fetchedIngData.push({
-            id: key,
-            amount: res.data[key].amount,
-            title: res.data[key].title,
-          });
-        }
-        // filterIngs(fetchedIngData);
-      } catch (err) {
-        console.log(err);
+    const timer = setTimeout(() => {
+      if (search === inputRef.current.value) {
+        const fetchData = async () => {
+          try {
+            const query =
+              search.length === 0 ? '' : `?orderBy="title"&equalTo="${search}"`;
+            const res = await axios.get(
+              'https://react-hooks-5d833-default-rtdb.firebaseio.com/ingredients.json' +
+                query
+            );
+            const fetchedIngData = [];
+            for (let key in res.data) {
+              fetchedIngData.push({
+                id: key,
+                amount: res.data[key].amount,
+                title: res.data[key].title,
+              });
+            }
+            filterIngs(fetchedIngData);
+          } catch (err) {
+            console.log(err);
+          }
+        };
+        fetchData();
       }
+    }, 500);
+    // useEffect can take a clean up function that runs before every useEffect
+    // return the clean up function
+    return () => {
+      clearTimeout(timer);
     };
-    fetchData();
-  }, [search, filterIngs]);
+  }, [search, filterIngs, inputRef]);
 
   return (
     <section className="search">
@@ -39,6 +49,7 @@ const Search = React.memo((props) => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             value={search}
             onChange={(event) => {
